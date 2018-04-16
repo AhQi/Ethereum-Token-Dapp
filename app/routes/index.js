8048c4d159eb7061b72004b46cb77f3c00e497cd
ahChi <ah941206@gmail.com>
2018-04-16 14:47:09 +0800

    style consistency modify

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,12 +3,11 @@ let path = process.cwd();
 const  MyTokenABI = require(path+'/contract/MyTokenAbi.js');
 
 module.exports = function (app, web3) {
+    let MyTokenContract = web3.eth.contract(MyTokenABI.ABI);
+    let MyToken = MyTokenContract.at(process.env.contractAddr);
     let accounts = web3.eth.accounts;
+
     web3.eth.defaultAccount = accounts[0];
-
-    var MyTokenContract = web3.eth.contract(MyTokenABI.ABI);
-
-    var MyToken = MyTokenContract.at(process.env.contractAddr);
 
     app.route('/GET/accounts')
         .get( function (req, res) {
@@ -21,8 +20,6 @@ module.exports = function (app, web3) {
 
     app.route('/POST/transfer')
         .post( function (req, res) {
-            console.log(accounts[req.body.accountId-1]);
-            console.log(req.body.amount);
 
             MyToken.transfer(accounts[req.body.accountId-1], req.body.amount)
             res.redirect('/');
@@ -31,10 +28,5 @@ module.exports = function (app, web3) {
     app.get("/", function (request, response) {
         response.sendFile(path + '/views/index.html');
     });
-
-
-
-
-
 
 };
